8cdf52979f9d08b133f14ceacc07a559194b328f
amanoizumi <a0918233246@gmail.com>
2023-04-01 14:17:48 +0800

    add: 安裝表單驗證套件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import { createPinia } from "pinia";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+// VeeValidate
+import { Form, Field, ErrorMessage, defineRule, configure } from "vee-validate";
+import AllRules from "@vee-validate/rules";
+import { localize, setLocale } from "@vee-validate/i18n";
+import zhTW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+
+// VeeValidate 定義規則
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule]);
+});
+
+// VeeValidate 設定
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+
+// 設定語系
+setLocale("zh_TW");
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -15,6 +35,10 @@ app.use(createPinia());
 
 app.use(VueAxios, axios);
 app.provide("axios", app.config.globalProperties.axios);
+
+app.component("FormView", Form);
+app.component("FieldView", Field);
+app.component("ErrorMessage", ErrorMessage);
 
 app.use(router);
 
---
+++ src/views/front/LoginView.vue
@@ -0,0 +1,22 @@
+<script>
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const getList = () => {};
+
+    onMounted(() => {
+      getList();
+    });
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>MetaWall</h1>
+    <p>到元宇宙展開全新社交圈</p>
+    <input type="email" />
+    <input type="password" />
+  </main>
+</template>
