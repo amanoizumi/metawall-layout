0489db49cb2623124543bdf6747262d3966a3781
amanoizumi <a0918233246@gmail.com>
2023-04-15 23:37:05 +0800

    add: 新增留言區

--- src/components/front/PostWall.vue
+++ src/components/front/PostWall.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { storeToRefs } from "pinia";
 
 import { timestampToDate } from "@/utils/toDate";
@@ -50,6 +50,51 @@ onMounted(() => {
       <article>
         <p>{{ post.content }}</p>
       </article>
+      <div class="flex items-center mb-[18px]">
+        <img
+          :src="post.user.photo"
+          class="w-10 h-10 object-cover rounded-full border-2 border-black mr-[10px]"
+        />
+        <div class="flex flex-1">
+          <input
+            v-model="commentInput"
+            class="input py-2 px-4"
+            type="text"
+            placeholder="留言..."
+          />
+          <button
+            type="button"
+            class="bg-primary text-white border-black border-2 border-l-0 p-2 hover:bg-accent hover:text-black shrink-0"
+          >
+            留言
+          </button>
+        </div>
+      </div>
+      <ul>
+        <li
+          class="bg-[#EFECE7] py-[18px] px-4 rounded-xl flex"
+          v-for="comment in post.comment"
+          :key="comment._id"
+        >
+          <img
+            :src="comment.user.photo"
+            class="w-10 h-10 object-cover rounded-full border-2 border-black mr-[10px]"
+          />
+          <div>
+            <a
+              href="#"
+              class="font-bold block hover:text-[#03438D] hover:underline"
+              >{{ comment.user.name }}</a
+            >
+            <time class="text-[#9B9893] text-xs mb-1">{{
+              timestampToDate(comment.createdAt)
+            }}</time>
+            <article>
+              <p>{{ comment.comment }}</p>
+            </article>
+          </div>
+        </li>
+      </ul>
     </li>
   </ul>
   <div v-else class="bg-white border-2 border-b-4 border-black rounded-lg">
