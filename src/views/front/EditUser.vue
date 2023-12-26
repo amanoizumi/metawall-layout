05652bca52948060796e0b2113fc26b48f903c9e
amanoizumi <a0918233246@gmail.com>
2023-12-26 12:17:30 +0800

    update: 加入 gh-pages 套件

--- src/views/front/EditUser.vue
+++ src/views/front/EditUser.vue
@@ -13,9 +13,11 @@ const router = useRouter();
 
 const postData = ref({});
 
-const createPost = async () => {
+const submitEditUser = async () => {
   console.log("送出更新");
 };
+
+console.log(userData);
 </script>
 
 <template>
@@ -27,6 +29,71 @@ const createPost = async () => {
       <h1 class="font-bold text-lg text-center">修改個人資料</h1>
     </div>
 
-    <div class="border-2 border-black bg-white rounded-lg p-8"></div>
+    <div>
+      <div class="flex pl-3">
+        <button
+          class="border-black border-2 border-b-0 rounded-t-lg px-8 py-2 bg-black text-white"
+          type="button"
+        >
+          暱稱修改
+        </button>
+        <button
+          class="border-black border-2 border-b-0 rounded-t-lg px-8 py-2 bg-white"
+          type="button"
+        >
+          重設密碼
+        </button>
+      </div>
+      <div class="border-2 border-black bg-white rounded-lg px-[106px] py-8">
+        <img
+          :src="userData.photo"
+          class="w-[107px] h-[107px] object-cover rounded-full border-2 border-black mx-auto mb-4"
+        />
+        <button
+          type="button"
+          class="block mx-auto py-2 px-8 bg-black text-white mb-3"
+        >
+          上傳大頭照
+        </button>
+
+        <VForm
+          class="flex flex-col gap-y-4"
+          ref="formDom"
+          v-slot="{ errors }"
+          @submit="submitEditUser"
+        >
+          <div>
+            <label for="nickName">暱稱</label>
+            <VField
+              id="nickName"
+              name="暱稱"
+              class="input mb-1"
+              type="text"
+              placeholder="暱稱"
+              rules="required|min:2"
+            />
+            <ErrorMessage
+              :class="{ 'text-[#F57375] block': errors['暱稱'] }"
+              name="暱稱"
+            ></ErrorMessage>
+          </div>
+          <div>
+            <label for="nickName">性別</label>
+            <VField
+              type="radio"
+              id="nickName"
+              name="性別"
+              class="w-5 h-5 text-black border-gray-300 focus:ring-primary focus:ring-2"
+              placeholder="性別"
+              rules="required|min:2"
+            />
+            <ErrorMessage
+              :class="{ 'text-[#F57375] block': errors['性別'] }"
+              name="性別"
+            ></ErrorMessage>
+          </div>
+        </VForm>
+      </div>
+    </div>
   </div>
 </template>
